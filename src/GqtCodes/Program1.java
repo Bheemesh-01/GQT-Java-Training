27300a2429dd4e79a10eccf2162bab6b0aeda15b
Bheemesh-47 <bh47rag@gmail.com>
2023-03-23 06:34:15 +0530

    added another print

--- src/GqtCodes/Program1.java
+++ src/GqtCodes/Program1.java
@@ -5,6 +5,7 @@ public class Program1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("hello world");
 	}
 
 }
